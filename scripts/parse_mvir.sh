1fdf75e909b4bb4c790796a06f7008fd25ddd12c
yuan_xy <yuan_xin_yu@hotmail.com>
2020-06-29 16:34:41 +0800

    peg grammar markdown file

--- scripts/parse_mvir.sh
+++ scripts/parse_mvir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.8 parse.py /code/LB/libra-vm/mol/compiler/ir_stdlib/modules/gas_schedule.mvir
+python3.8 parse.py /code/LB/libra-vm/mol/compiler/ir_stdlib/modules/gas_schedule.mvir -vvvv
